ed0d6a7804599659761899ca0b3ce2843e27e4bd
Stefano Casasola <ste.casasola@gmail.com>
2020-11-23 12:05:04 +0100

    Adds loading indicator, improves UX

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 class Movie extends React.Component {
 
     state = {
-        movieDetails: {}
+        movieDetails: {},
+        loading: true,
     }
 
     componentDidMount = async () => {
@@ -13,17 +14,32 @@ class Movie extends React.Component {
             let response = await fetch("http://www.omdbapi.com/?apikey=24ad60e9&s=" + this.props.movieTitle)
             if (response.ok) {
                 let results = await response.json()
-                console.log(results)
-                this.setState({ movieDetails: results.Search[0] })
+                // console.log(results)
+                setTimeout(() => {
+                    this.setState({ movieDetails: results.Search[0], loading: false })
+                }, 1000)
+            } else {
+                console.log('an error happened!')
+                this.setState({ loading: false })
             }
         } catch (e) {
             console.log(e)
+            this.setState({ loading: false })
         }
 
     }
 
     render() {
-        return <h1>Loading...</h1>
+        return (
+            <div>
+                <h1>
+                    {this.state.loading ? 'Loading...' : this.state.movieDetails.Title}
+                </h1>
+                {!this.state.loading &&
+                    <img src={this.state.movieDetails.Poster} alt="movie poster" style={{ width: '200px' }} />
+                }
+            </div>
+        )
     }
 }
 
